b624f34785956ea9b8d5784f9f88a5b4b10a7ba9
MinhNhat <jimtruong13092003@gmail.com>
2023-10-21 07:21:24 +0700

    Build base project

---
+++ src/main/java/dev/minhnhat/week06_lab_truongduongminhnhat_21028411/Week06LabTruongDuongMinhNhat21028411Application.java
@@ -0,0 +1,13 @@
+package dev.minhnhat.week06_lab_truongduongminhnhat_21028411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week06LabTruongDuongMinhNhat21028411Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week06LabTruongDuongMinhNhat21028411Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/minhnhat/week06_lab_truongduongminhnhat_21028411/Week06LabTruongDuongMinhNhat21028411ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.minhnhat.week06_lab_truongduongminhnhat_21028411;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week06LabTruongDuongMinhNhat21028411ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
